43b008af8d20b03efb084d915b9246765101deec
Khilkos <147064658+Khilkos@users.noreply.github.com>
2023-10-10 16:58:12 +0300

    Create Font_table.h

---
+++ My_Libraries/Font_table.h
@@ -0,0 +1,27 @@
+/*
+ * LCD_scr.h
+ *
+ * Created: 19.02.2023 13:40:09
+ *  Author: dasha
+ */ 
+
+
+#ifndef Font_table_H_
+#define Font_table_H_
+
+
+extern const unsigned char FontTable[256][5];
+extern const unsigned char D_0[8];
+extern const unsigned char D_1[8];
+extern const unsigned char D_2[8];
+extern const unsigned char D_3[8];
+extern const unsigned char D_4[8];
+extern const unsigned char D_5[8];
+extern const unsigned char D_6[8];
+extern const unsigned char D_7[8];
+extern const unsigned char D_8[8];
+extern const unsigned char D_9[8];
+
+#endif /* LCD_SCR_H_ */
+
+
