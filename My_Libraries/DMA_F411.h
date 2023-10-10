588987a77e34d13bd3a6be48cefbbb9d9bcf081c
Khilkos <147064658+Khilkos@users.noreply.github.com>
2023-10-10 16:57:50 +0300

    Create DMA_F411.h

---
+++ My_Libraries/DMA_F411.h
@@ -0,0 +1,26 @@
+#ifndef __DMA_F411_H
+#define __DMA_F411_H
+
+#include "stm32f411xe.h"
+#include "stdint.h"
+//#include "cmsis_armclang.h" 
+//#include "core_cm4.h"
+#include "ADC_F411.h"
+#include "DS18B20_F411.h"
+
+
+#define res 16
+
+
+extern uint16_t ADC_DMA_val[2];
+
+void DMA_F411_init (void);
+void DMA_F411_ADC_init (void);
+void DMA2_Stream0_IRQHandler(void);
+void DMA_F411_DS18B20_convert_T (void);
+void DMA1_Stream6_IRQHandler(void);
+void DMA_F411_DS18B20_read_scratch (void);
+void DMA1_Stream5_IRQHandler(void);
+void DMA_read_test (void);
+
+#endif
