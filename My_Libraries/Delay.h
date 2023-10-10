ee8ee8f0ff00b804c70d4bd039a6a5cd21337c10
Khilkos <147064658+Khilkos@users.noreply.github.com>
2023-10-10 16:57:39 +0300

    Create Delay.h

---
+++ My_Libraries/Delay.h
@@ -0,0 +1,26 @@
+#ifndef __Delay_H
+#define __Delay_H
+
+/*
+Libraries for delay in ms and us
+need degined in mail programm SysTick();
+*/
+
+
+#include "stm32f411xe.h"
+#include "stdint.h"
+#include "cmsis_armclang.h" 
+#include "core_cm4.h"
+#include "DS3231_F411.h"
+
+static uint32_t count=0;
+extern uint32_t key_delay;
+extern uint32_t select_delay;
+
+void delay_us(uint32_t);
+void delay_ms(uint32_t);
+void SysTick_Handler(void);
+void SysTick_Init(void);
+
+#endif
+
