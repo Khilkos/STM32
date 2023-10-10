c139d088897eb69ba424c568c37e14950d86bf58
Khilkos <147064658+Khilkos@users.noreply.github.com>
2023-10-10 16:59:10 +0300

    Create SPI_F411.h

---
+++ My_Libraries/SPI_F411.h
@@ -0,0 +1,19 @@
+#ifndef __SPI_F411_H
+#define __SPI_F411_H
+
+#include "stm32f411xe.h"
+//#include "stdint.h"
+//#include "cmsis_armclang.h" 
+//#include "core_cm4.h"
+
+#define res 16
+
+void SPI_F411_init(void);
+
+
+#endif
+
+
+
+
+
