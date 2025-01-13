0b9770c49e30bc79574a83de7a70b08365d7a17f
Khilkos <147064658+Khilkos@users.noreply.github.com>
2025-01-13 17:35:13 +0300

    3

--- Keil/Amur_750/main.c
+++ Keil/Amur_750/main.c
@@ -11,12 +11,14 @@ GPIO_DO_setup(GPIOE,0,High);
 		
 GPIO_Alternate(GPIOA,7,Push_pull,High,No_pull,AF5);// SPI MOSI_1
 GPIO_Alternate(GPIOA,5,Push_pull,High,No_pull,AF5);// SPI CLK_1
-//GPIO_Alternate(GPIOA,4,Push_pull,High,Pull_up,AF5);// SPI NSS_1
+GPIO_Alternate(GPIOA,4,Push_pull,High,Pull_up,AF5);// SPI NSS_1
  
 
 SysTick_H7_Init(456);	
 Core_STM32_H7_init(1,114,2,19,2);			
 SPI1_init();		
+
+//SPI1->CR1 |= (SPI_CR1_CSTART);			
 		
 		while (1)
 	{
@@ -28,10 +30,13 @@ GPIOE->BSRR=1<<(0);
 //delay_us (50);		
 	//SPI1->DR=temp;
 //SPI1->TXDR = 0b10101010;	
-SPI1->TXDR =0xF0F0f0f0;		
+SPI1->TXDR =0x00000000;		
+SPI1->TXDR =0xFfFfffff;
+SPI1->TXDR =0x00000000;		
+SPI1->TXDR =0xFfFfffff;		
 SPI1->CR1 |= (SPI_CR1_CSTART);		
-while (!(SPI1->SR&SPI_SR_TXC)) {__NOP();}
-delay_us(10);
+while (!(SPI1->SR&SPI_SR_EOT)) {__NOP();}
+//delay_us(10);
 		
 	}
 	
