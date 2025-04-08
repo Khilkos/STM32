f817d5c159c13bcb1aefb66d6dc5644c00d44b4c
Khilkos <147064658+Khilkos@users.noreply.github.com>
2025-04-08 12:11:39 +0300

    3

--- Keil/Amur_750/Objects/main.d
+++ Keil/Amur_750/Objects/main.d
@@ -1,14 +1,14 @@
 ./objects/main.o: main.c ..\Amur_750\main.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h ..\Amur_750\Config.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.1.1\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h750xx.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm7.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cachel1_armv7.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.1.1\Drivers\CMSIS\Device\ST\STM32H7xx\Include\system_stm32h7xx.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.1.1\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h750xx.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm7.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cachel1_armv7.h \
+  C:\Users\Sergey\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.1.1\Drivers\CMSIS\Device\ST\STM32H7xx\Include\system_stm32h7xx.h \
   ..\..\My_Libraries\Delay.h ..\..\My_Libraries\GPIO_setup.h \
   ..\..\My_Libraries\Core_init.h ..\..\My_Libraries\SPI.h \
   ..\..\My_Libraries\DMA.h ..\..\My_Libraries\Timer.h
--- Keil/Amur_750/RTE/_Target_1/RTE_Components.h
+++ Keil/Amur_750/RTE/_Target_1/RTE_Components.h
@@ -10,6 +10,12 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
 /* Keil::Device:Startup:1.11.0 */
 #define RTE_DEVICE_STARTUP_STM32H7XX    /* Device Startup for STM32H7 */
 
