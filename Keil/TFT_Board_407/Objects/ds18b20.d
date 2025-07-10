37f8a2b39be69a840c99a7e386c3cd06278cbbcb
Khilkos <147064658+Khilkos@users.noreply.github.com>
2025-07-10 21:54:41 +0300

    1

--- Keil/TFT_Board_407/Objects/ds18b20.d
+++ Keil/TFT_Board_407/Objects/ds18b20.d
@@ -1,8 +1,8 @@
 ./objects/ds18b20.o: ..\..\My_Libraries\DS18B20.c \
   ..\..\My_Libraries\DS18B20.h ..\TFT_Board_407\Config.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\Khilk\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
+  C:\Users\dasha\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
+  C:\Users\dasha\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\dasha\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
   ..\..\My_Libraries\Core_init.h ..\..\My_Libraries\Delay.h \
   ..\..\My_Libraries\GPIO_setup.h ..\..\My_Libraries\Timer.h \
   ..\..\My_Libraries\FSMC.h ..\..\My_Libraries\SSD1963.h \
